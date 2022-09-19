57321abd8f6d74836de02df4c9e8c3185a702e60
mendokusaiiii <victor_cabral_o@hotmail.com>
2022-09-19 17:08:47 -0300

    req 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import FavoritePokemons from '../pages/FavoritePokemons';
+import renderWithRouter from '../helpers/renderWithRouter';
+
+describe('Testando o componente FavoritePokemons', () => {
+  test('Testa se é exibido na tela a mensagem', () => {
+    renderWithRouter(<FavoritePokemons />);
+
+    const noFavorite = screen.getByText(/No favorite pokemon found/i);
+    expect(noFavorite).toBeInTheDocument();
+  });
+});
