be78948852eaffab13889a8c1d8850d4ebfe0827
mendokusaiiii <victor_cabral_o@hotmail.com>
2022-09-19 17:01:30 -0300

    req 1 e 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import About from '../pages/About';
+import renderWithRouter from '../helpers/renderWithRouter';
+
+describe('Testando o componente About', () => {
+  test('Se a pagina contém informações da Pokedex', () => {
+    renderWithRouter(<About />);
+
+    const aboutPokedex = screen.getByRole(
+      'heading',
+      { level: 2, name: /About Pokédex/i },
+    );
+    expect(aboutPokedex).toBeInTheDocument();
+
+    const pokedexImg = screen.getByRole('img', { name: /Pokédex/i });
+    expect(pokedexImg).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,55 @@
-test('', () => {});
+import React from 'react';
+import { screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { act } from 'react-dom/test-utils';
+import renderWithRouter from '../helpers/renderWithRouter';
+import App from '../App';
+
+describe('Testando os componentes da aplicação', () => {
+  test('Se App renderiza', () => {
+    renderWithRouter(<App />);
+
+    const renderHome = screen.getByRole('link', { name: /Home/i });
+    expect(renderHome).toBeInTheDocument();
+
+    const renderAbout = screen.getByRole('link', { name: /About/i });
+    expect(renderAbout).toBeInTheDocument();
+
+    const renderFavorite = screen.getByRole('link', { name: /Favorite/i });
+    expect(renderFavorite).toBeInTheDocument();
+  });
+  test('Se redireciona para a pagina inicial', () => {
+    const { history } = renderWithRouter(<App />);
+    const renderHome = screen.getByRole('link', { name: /Home/i });
+    userEvent.click(renderHome);
+    expect(history.location.pathname).toBe('/');
+  });
+  test('Se redireciona para About', async () => {
+    const { history } = renderWithRouter(<App />);
+    const renderAbout = screen.getByRole('link', { name: /about/i });
+    userEvent.click(renderAbout);
+    await waitFor(() => expect(history.location.pathname).toBe('/about'));
+  });
+  test('Se redireciona para os favoritos', async () => {
+    const { history } = renderWithRouter(<App />);
+
+    const renderFavorite = screen.getByRole('link', { name: /favorite pokémons/i });
+
+    userEvent.click(renderFavorite);
+    await waitFor(() => expect(history.location.pathname).toBe('/favorites'));
+  });
+  test('Se redireciona para NotFound', () => {
+    const { history } = renderWithRouter(<App />);
+    act(() => {
+      history.push('/**');
+    });
+    expect(history.location.pathname).toBe('/**');
+    const notFound = screen.getByRole('heading', { name: /page requested not found/i });
+    const notFoundPikachu = screen.getByRole(
+      'img',
+      { name: /pikachu crying because the page requested was not found/i },
+    );
+    expect(notFound).toBeInTheDocument();
+    expect(notFoundPikachu).toBeInTheDocument();
+  });
+});
