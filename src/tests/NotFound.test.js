dc5173d043bc853a87c860d0a9d166fea2cdefd0
mendokusaiiii <victor_cabral_o@hotmail.com>
2022-09-19 17:23:55 -0300

    req 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import NotFound from '../pages/NotFound';
+import renderWithRouter from '../helpers/renderWithRouter';
+
+describe('Testando o componenete NotFound', () => {
+  test('Testa se a pagina contém um heading', () => {
+    renderWithRouter(<NotFound />);
+    const notFound = screen.getByRole(
+      'heading',
+      { level: 2, name: /Page requested not found/i },
+    );
+    expect(notFound).toBeInTheDocument();
+  });
+  test('Testa se a pagina mostra um imagem', () => {
+    renderWithRouter(<NotFound />);
+    const screenImgNotFound = screen.getByRole(
+      'img',
+      { name: /Pikachu crying because the page requested was not found/i },
+    );
+    expect(screenImgNotFound).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
